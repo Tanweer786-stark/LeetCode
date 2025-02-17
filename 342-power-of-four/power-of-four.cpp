b6d6322e930f2af88e727be48c02beeb3c16a61c
Tanweer ibnhaider <118075528+Tanweer786-stark@users.noreply.github.com>
2025-02-17 23:34:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (53.51%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -4,6 +4,6 @@ public:
         if(n==1) return true;
         if(n<=0) return false;  //Recursion base Corner Condition
         if(n%4!=0) return false;
-        return isPowerOfFour(n/4); //Not effect on ti,e complexity
+        return isPowerOfFour(n/4); //Not effect on time complexity
     }
 };
